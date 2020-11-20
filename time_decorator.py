e1416ea5ec58511d52f3f88b547f380e1ee11550
Shaun <shaunlipy@gmail.com>
2020-11-19 21:28:58 -0500

    Create time_decorator.py

---
+++ time_decorator.py
@@ -0,0 +1,22 @@
+import time
+from functools import wraps
+
+
+def timeit(method):
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = method(*args, **kwargs)
+        end_time = time.time()
+        print(f"{method.__name__} => {(end_time-start_time)*1000} ms")
+
+        return result
+
+    return wrapper
+    
+@timeit
+def func(n):
+    return list(range(n))
+    
+if __name__ == '__main__':
+  res = func(3)
